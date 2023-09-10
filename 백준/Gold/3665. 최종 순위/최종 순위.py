fc7ef3b301af53d7d69fc91159a4a41f75f3887b
YunaDaddy <67175445+holrosky@users.noreply.github.com>
2023-09-11 00:32:47 +0900

    [Gold I] Title: 최종 순위, Time: 572 ms, Memory: 41924 KB -BaekjoonHub

--- 백준/Gold/3665. 최종 순위/최종 순위.py
+++ 백준/Gold/3665. 최종 순위/최종 순위.py
@@ -1,68 +1,82 @@
-from collections import deque
 import sys
+from collections import deque
 
-t = int(sys.stdin.readline())
+input = sys.stdin.readline
 
-for i in range(t):
-    n = int(sys.stdin.readline())
+tc = int(input().strip())
 
-    graph = [[] for _ in range(n + 1)]
-    inDegree = [0 for _ in range(n + 1)]
+for _ in range(tc):
+    n = int(input().strip())
+
+    nodes = list(map(int, input().split()))
+    m = int(input().strip())
+
+    graph = [set() for _ in range(n+1)]
+
+    for i in range(n):
+        for j in range(i+1, n):
+            graph[nodes[i]].add(nodes[j])
+
+    indegree = [n-(len(x)+1) for x in graph]
+
+
+    for _ in range(m):
+        a, b = map(int, input().split())
+        if a in graph[b]:
+            graph[a].add(b)
+            graph[b].remove(a)
+
+            indegree[a] -= 1
+            indegree[b] += 1
+        else:
+            graph[b].add(a)
+            graph[a].remove(b)
+
+            indegree[b] -= 1
+            indegree[a] += 1
+
     queue = deque()
-    answer = []
-    flag = 0
 
-    team = list(map(int, sys.stdin.readline().rstrip().split()))
+    error = False
 
-    for j in range(n - 1):
-        for k in range(j + 1, n):
-            graph[team[j]].append(team[k])
-            inDegree[team[k]] += 1
+    cnt = 0
+    for i in range(1, n+1):
+        if not indegree[i]:
+            cnt += 1
+            queue.append(i)
 
-    m = int(sys.stdin.readline())
-    for j in range(m):
-        first, second = map(int, sys.stdin.readline().rstrip().split())
-        flag = True
-
-        for k in graph[first]:
-            if k == second:
-                graph[first].remove(second)
-                inDegree[second] -= 1
-                graph[second].append(first)
-                inDegree[first] += 1
-                flag = False
-
-        if flag:
-            graph[second].remove(first)
-            inDegree[first] -= 1
-            graph[first].append(second)
-            inDegree[second] += 1
-
-    for j in range(1, n + 1):
-        if inDegree[j] == 0:
-            queue.append(j)
-
-    if not queue:
-        print("IMPOSSIBLE")
-        continue
-
-    result = True
-    while queue:
-        if len(queue) > 1:
-            result = False
-            break
-
-        tmp = queue.popleft()
-        answer.append(tmp)
-        for j in graph[tmp]:
-            inDegree[j] -= 1
-            if inDegree[j] == 0:
-                queue.append(j)
-            elif inDegree[j] < 0:
-                result = False
+            if cnt >= 2:
+                error = True
                 break
 
-    if not result or len(answer) < n:
-        print("IMPOSSIBLE")
+        if error:
+            break
+
+    if error:
+        print('?')
+        continue
+
+    error = False
+    res = []
+    while queue:
+        node = queue.popleft()
+        res.append(str(node))
+
+        cnt = 0
+        for each in graph[node]:
+            indegree[each] -= 1
+
+            if not indegree[each]:
+                cnt += 1
+                queue.append(each)
+
+        if cnt >= 2:
+            error = True
+            break
+
+    if error:
+        print('?')
+    elif not res or len(res) < n:
+        print('IMPOSSIBLE')
     else:
-        print(*answer)
+        print(' '.join(res))
